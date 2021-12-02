773447b0634cb766656f7e3cc508dc09f2e6ff59
Zhe Miao <2598803M@student.gla.ac.uk>
2021-12-02 20:17:25 +0800

    update rate function (back end part, user can get his rating dynamically)

---
+++ wind/src/main/java/com/glasgow/wind/controller/RatingController.java
@@ -0,0 +1,42 @@
+package com.glasgow.wind.controller;
+
+import com.glasgow.wind.domain.Rating;
+import com.glasgow.wind.service.RatingService;
+import com.glasgow.wind.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author Zhe Miao Guid: 2598803M
+ * @Date 2021/11/30 18:03
+ * @Version 1.0
+ */
+
+@Controller
+@RequestMapping("/rate")
+public class RatingController {
+    @Autowired
+    RatingService ratingService;
+
+    @GetMapping("/{albumId}/{userId}")
+    @ResponseBody
+    public Object getRatingByAlbumIdAndUserId(@PathVariable("albumId") int albumId, @PathVariable("userId") int userId, Model model){
+        List<Rating> ratings = ratingService.queryByAlbumIdAndUserId(albumId, userId);
+        if(ratings.size() != 1){
+            return ResponseUtil.fail();
+        }
+
+        return ResponseUtil.ok(ratings.get(0).getScore());
+    }
+
+    @PostMapping("/create")
+    @ResponseBody
+    public Object create(@RequestBody Map<String,String> map){
+        return ResponseUtil.ok();
+    }
+}
---
+++ wind/src/main/java/com/glasgow/wind/service/RatingService.java
@@ -0,0 +1,29 @@
+package com.glasgow.wind.service;
+
+import com.glasgow.wind.dao.RatingMapper;
+import com.glasgow.wind.domain.Rating;
+import com.glasgow.wind.domain.RatingExample;
+import com.glasgow.wind.domain.UserExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Author Zhe Miao Guid: 2598803M
+ * @Date 2021/12/2 20:01
+ * @Version 1.0
+ */
+
+@Service
+public class RatingService {
+    @Autowired(required=false)
+    RatingMapper ratingMapper;
+
+    public List<Rating> queryByAlbumIdAndUserId(int albumId, int userId){
+        RatingExample example = new RatingExample();
+        example.createCriteria().andAlbumIdEqualTo(albumId).andUserIdEqualTo(userId);
+
+        return ratingMapper.selectByExample(example);
+    }
+}
